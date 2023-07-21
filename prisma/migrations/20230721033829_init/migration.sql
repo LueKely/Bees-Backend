575885ab53980f6851c515ea4a0076713e7dd422
LueKely <luekely43110@gmail.com>
2023-07-21 11:40:03 +0800

    DB-Connections

---
+++ prisma/migrations/20230721033829_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "user_id" INTEGER NOT NULL,
+    "session_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "ChatRoom" (
+    "room_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+
+    CONSTRAINT "ChatRoom_pkey" PRIMARY KEY ("room_id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "message_id" INTEGER NOT NULL,
+    "room_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "content" TEXT,
+    "sent_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("message_id")
+);
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "participant_id" INTEGER NOT NULL,
+    "room_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "joined_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("participant_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_room_id_fkey" FOREIGN KEY ("room_id") REFERENCES "ChatRoom"("room_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Session"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_room_id_fkey" FOREIGN KEY ("room_id") REFERENCES "ChatRoom"("room_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Session"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 const app = express();
 const port = 3000;
 
 // Define your routes and middleware here
+const prisma = new PrismaClient();
+async function testPrismaConnection() {
+	try {
+		await prisma.$connect();
+		console.log('Connected to the database!');
+	} catch (error) {
+		console.error('Failed to connect to the database:', error);
+	} finally {
+		await prisma.$disconnect();
+	}
+}
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Goo Goo gaa gaa i am a babywaby');
@@ -10,4 +22,5 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
+	testPrismaConnection();
 });
