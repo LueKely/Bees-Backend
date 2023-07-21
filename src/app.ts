0cd22852f8842dd77c9654dc9f3665177fbfec89
LueKely <luekely43110@gmail.com>
2023-07-21 12:28:20 +0800

    Build: adjusted config

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,6 @@ const port = 3000;
 
 // Define your routes and middleware here
 const prisma = new PrismaClient();
-async function testPrismaConnection() {
-	try {
-		await prisma.$connect();
-		console.log('Connected to the database!');
-	} catch (error) {
-		console.error('Failed to connect to the database:', error);
-	} finally {
-		await prisma.$disconnect();
-	}
-}
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Goo Goo gaa gaa i am a babywaby');
@@ -22,5 +12,4 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
-	testPrismaConnection();
 });
