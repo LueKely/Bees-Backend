799499dd41d5c4329e90731a43dd4adf857ef699
Anunciacion Lue <luekely43110@gmail.com>
2023-07-26 22:17:35 +0800

    feat: socket cringe middleware

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,13 @@ import sessions from './routes/session';
 import room from './routes/rooms';
 import chat from './routes/chat';
 
+import cookie from 'cookie';
+
 const app = express();
 const port = 3000;
 
 const httpServer = createServer(app);
+
 const io = new Server(httpServer, {
 	cors: { origin: ['http://localhost:3030'] },
 });
@@ -24,12 +27,42 @@ io.use((socket: Socket, next) => {
 		console.log(`Incoming event: ${event}`);
 		console.log(`Data received: ${JSON.stringify(args)}`);
 	});
+
+	if (!socket.handshake.headers.cookie) {
+		const error = new Error('Unauthorized');
+		return next(error);
+	}
+
+	const cookies = cookie.parse(socket.handshake.headers.cookie);
+
+	// Check if the cookie exists and if it is expired
+	if (cookies.session) {
+		const expirationTimestamp = parseInt(cookies.session, 10);
+		const currentTime = Math.floor(Date.now() / 1000); // Convert to UNIX timestamp
+
+		if (currentTime > expirationTimestamp) {
+			const error = new Error('Cookie is expired');
+			return next(error);
+		}
+	}
+
+	console.log(cookies.session); // prints le cookie
+
 	next();
 });
 
 // put all the the shit here
 io.on('connection', (socket: Socket) => {
 	// sends messages to all the people who are listining to it
+	socket.on('join-room', (room) => {
+		// Join the room
+		socket.join(room);
+
+		// You can perform additional actions here when the client joins the room
+
+		console.log(`Socket ${socket.id} joined room: ${room}`);
+	});
+
 	socket.on('send-message', (message, room) => {
 		socket.to(room).emit('recieve-message', message);
 	});
