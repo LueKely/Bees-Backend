6f22bc0dd2aea2f1561e11503da61a1366c9ee8f
LueKely <luekely43110@gmail.com>
2023-07-26 13:50:08 +0800

    feat: initial socket connections

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,49 @@
 import express, { Request, Response } from 'express';
-import sessions from './routes/session';
-import room from './routes/rooms';
-import chat from './routes/chat';
 import cookieParser from 'cookie-parser';
 import 'express-async-errors';
 
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { Socket } from 'socket.io';
+
+import sessions from './routes/session';
+import room from './routes/rooms';
+import chat from './routes/chat';
+
 const app = express();
 const port = 3000;
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: { origin: ['http://localhost:3030'] },
+});
+
+// middleware
+io.use((socket: Socket, next) => {
+	socket.onAny((event, ...args) => {
+		console.log(`Incoming event: ${event}`);
+		console.log(`Data received: ${JSON.stringify(args)}`);
+	});
+	next();
+});
+
+// put all the the shit here
+io.on('connection', (socket: Socket) => {
+	// sends messages to all the people who are listining to it
+	socket.on('send-message', (message, room) => {
+		socket.to(room).emit('recieve-message', message);
+	});
+	// sends how many shemay are there in the room
+	socket.on('add-pariticipant', (room) => {
+		socket.to(room).emit('increment-pariticipant');
+	});
+});
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Goo Goo gaa gaa i am a babywaby');
 });
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
 
